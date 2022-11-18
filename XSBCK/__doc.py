ea94738f90cebb49f4c1c634d8f0562c37a96aaf
yrobin <yoann.robin.k@gmail.com>
2022-11-18 12:47:13 +0100

    Update doc

--- XSBCK/__doc.py
+++ XSBCK/__doc.py
@@ -38,6 +38,7 @@ XSBCK ({})
 
 {}
 
+
 Input parameters
 ----------------
 --log Nothing, 'debug', 'info', 'warning', 'error' or 'critical'
@@ -49,8 +50,8 @@ Input parameters
     A list of netcdf input files, as reference
 --input-biased, -ibias, -iX
     A list of netcdf input files, as model to correct
---output-corrected, -ocorr, -oZ
-    A list of netcdf output files
+--output-dir, -odir, -oZ
+    A path to save the corrected data. The name of input-biased is used.
 --method
     Bias correction method
 --n-workers [int]
@@ -61,17 +62,15 @@ Input parameters
     Memory per worker
 --tmp [str, default is tempfile.gettempdir()]
     Base path to build a random temporary folder.
+--window [w_left,w_predict,w_right, default=5,10,5]
+    The moving window for the correction: w_left + w_predict + w_right is the
+    fit window, whereas w_predict is the correction part.
 --chunks [chunk_lat,chunk_lon]
     Spatial chunk of the dataset. If not given, default value is:
     chunk_lat = int(nlat / sqrt(n_threads))
     chunk_lon = int(nlon / sqrt(n_threads))
---window [w_left,w_predict,w_right, default=5,10,5]
-    The moving window for the correction: w_left + w_predict + w_right is the
-    fit window, whereas w_predict is the correction part.
 --calibration [default= "1976/2005]
     Calibration period, in the form 'year_start/year_end'.
---disable-dask
-    Use this option to disable the dask client.
 --cvarsX
     Model climate variables, in the form var0,var1,var2,...
 --cvarsY
@@ -79,15 +78,55 @@ Input parameters
 --cvarsZ
     Name of output variables, in the form var0,var1,var2,... If not given,
     cvarsX is used.
+--ppp
+    A list of PrePostProcessing methods.
+--disable-dask
+    Use this option to disable the dask client.
+
 
 Examples
 --------
-xsbck --log -iY in1o.nc in2o.nc in3o.nc -iX in1m.nc in2m.nc in3m.nc -oZ out1.nc out2.nc out3.nc --method R2D2-L-NV-2L --n-workers 5 --threads-per-worker 2 --memory 3gb
+xsbck --log -iref $ipathY/*.nc -ibias $ipathX/*.nc -odir $opathZ/\\
+   --method R2D2-L-NV-2L\\
+   --n-workers 5 --threads-per-worker 2\\
+   --memory 2GB\\
+   --window 5,50,5\\
+   --cvarsX tas,tasmin,tasmax,pr\\
+   --cvarsY tas,tasmin,tasmax,prtot\\
+   --cvarsZ tas,tasmin,tasmax,prtot\\
+   --ppp '_all_,LogLin[cols=prtot+tasmin+tasmax]'\\
+   --ppp prtot,SSR\\
+   --ppp '_all_,DiffRef[ref=tas,upper=tasmax,lower=tasmin],NotFiniteAnalog[analog_var=tas+prtot,threshold=0.05]'
 
+
+About ppp
+---------
+The ppp arguments are PrePostProcessing class, in the SBCK.ppp package. You can
+use it in XSBCK with the following rule:
+- ppp are applied as a function (so right to left) in the order of the
+  arguments passed to xsbck.  For the behind example, before the correction,
+  ppp are applied in this order:
+  * NotFiniteAnalog
+  * DiffRef
+  * SSR
+  * LogLin
+  And in the reverse order after the correction.
+- a ppp:
+  * start with the variable to applied,
+  * and after the ',', the list of SSR to apply is given.
+  * You can use [] to pass arguments to the ppp,
+  * An you can use _all_ as variable to indicates the ppp applied to all
+  variables.
+In the example behind, we use the ppp 'NotFiniteAnalog' to replace invalid
+values in tasmin, tasmax by the closer analog defined by tas and prtot, if the
+proportion is lower < 5%. Then, tasmax is corrected as tasmax-tas, and tasmin
+as tas-tasmin. The SSR is used for prtot, and the LogLin function is applied to
+SSR(prtot), tasmax-tas and tas-tasmin (because they are non zero).
 
 License {}
 {}
 {}
+
 
 Sources and author(s)
 ---------------------
