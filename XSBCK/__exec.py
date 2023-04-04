d4233e5aa55c2a37d9178a1f7ef8084e3920e138
yrobin <yoann.robin.k@gmail.com>
2023-04-04 08:38:16 +0200

    Not work, but use the new interface

--- XSBCK/__exec.py
+++ XSBCK/__exec.py
@@ -38,18 +38,15 @@ import zarr
 ## Imports ##
 #############
 
+
+from .__XSBCKParams import xsbckParams
+from .__exceptions  import AbortForHelpException
+
 from .__logs import LINE
-from .__logs import init_logging
+from .__logs    import log_start_end
 
 from .__release    import version
 from .__curses_doc import print_doc
-
-from .__input import read_inputs
-from .__input import check_inputs
-
-from .__exceptions import AbortException
-
-from .__tmp import build_tmp_dir
 
 from .__io import load_data
 from .__io import save_data
@@ -73,7 +70,8 @@ logger.addHandler(logging.NullHandler())
 ## Functions ##
 ###############
 
-def run_xsbck( kwargs ):##{{{
+@log_start_end(logger)
+def run_xsbck():##{{{
 	"""
 	XSBCK.run_xsbck
 	===============
@@ -82,19 +80,9 @@ def run_xsbck( kwargs ):##{{{
 	
 	"""
 	
-	logger.info( "XSBCK:start" )
-	
-	
-	## Init the distributed client
-	if not kwargs["disable_dask"]:
-		dask.config.set( temporary_directory = kwargs["tmp_dask"] )
-#		dask.config.set( **{ "temporary_directory" : kwargs["tmp_dask"] , 'array.slicing.split_large_chunks' : False } )
-		kwargs_client = { "n_workers"          : kwargs["n_workers"] ,
-		                  "threads_per_worker" : kwargs["threads_per_worker"] ,
-		                  "memory_limit"       : f"{int(kwargs['memory_per_worker'].B)}B" }
-		wclient = distributed.Client(**kwargs_client)
-	
+	xsbckParams.init_dask()
 	try:
+		raise Exception("Stop by user")
 		## Load data
 		zX,zY,zZ = load_data(kwargs)
 		
@@ -110,12 +98,8 @@ def run_xsbck( kwargs ):##{{{
 	except Exception as e:
 		raise e
 	finally:
-		if not kwargs["disable_dask"]:
-			wclient.close()
-			del wclient
+		xsbckParams.stop_dask()
 	
-	logger.info( "XSBCK:end" )
-	logger.info(LINE)
 ##}}}
 
 def start_xsbck(*argv):##{{{
@@ -130,57 +114,66 @@ def start_xsbck(*argv):##{{{
 	walltime0 = dt.datetime.utcnow()
 	
 	## Read input
-	kwargs = read_inputs(*argv)
+	xsbckParams.init_from_user_input(*argv)
 	
 	## Init logs
-	init_logging(kwargs["log"])
+	xsbckParams.init_logging()
+	
+	## Logging
 	logger.info(LINE)
-	logger.info( "Start: {}".format(str(walltime0)[:19] + " (UTC)") )
+	logger.info( "Start: {}".format( str(walltime0)[:19] + " (UTC)") )
 	logger.info(LINE)
 	
 	## Package version
+	pkgs = [("SBCK"       , bc ),
+	        ("numpy"      , np ),
+	        ("xarray"     , xr ),
+	        ("dask"       , dask ),
+	        ("distributed", distributed ),
+	        ("zarr"       , zarr ),
+	        ("netCDF4"    , netCDF4 )
+	       ]
+	
 	logger.info( "Packages version:" )
 	logger.info( " * {:{fill}{align}{n}}".format( "XSBCK" , fill = " " , align = "<" , n = 12 ) + f"version {version}" )
-	for name_pkg,pkg in zip(["SBCK","numpy","xarray","dask","distributed","zarr","netCDF4"],[bc,np,xr,dask,distributed,zarr,netCDF4]):
+	for name_pkg,pkg in pkgs:
 		logger.info( " * {:{fill}{align}{n}}".format( name_pkg , fill = " " , align = "<" , n = 12 ) +  f"version {pkg.__version__}" )
 	logger.info(LINE)
 	
 	## Serious functions start here
 	try:
-		## Build temporary
-		build_tmp_dir(kwargs)
+		
+		## Check inputs
+		xsbckParams.check()
+		
+		## Init temporary
+		xsbckParams.init_tmp()
 		
 		## List of all input
 		logger.info("Input parameters:")
-		keys = [key for key in kwargs]
-		keys.sort()
-		for key in keys:
-			logger.info( " * {:{fill}{align}{n}}".format( key , fill = " ",align = "<" , n = 10 ) + ": {}".format(kwargs[key]) )
-		logger.info(LINE)
-		
-		## Check inputs
-		abort = check_inputs(kwargs)
+		for key in xsbckParams.keys():
+			logger.info( " * {:{fill}{align}{n}}".format( key , fill = " ",align = "<" , n = 10 ) + ": {}".format(xsbckParams[key]) )
 		logger.info(LINE)
 		
 		## User asks help
-		if kwargs["help"]:
+		if xsbckParams.help:
 			print_doc()
-			abort = True
 		
 		## In case of abort, raise Exception
-		if abort:
-			raise AbortException
+		if xsbckParams.abort:
+			raise xsbckParams.error
 		
 		## Go!
-		run_xsbck(kwargs)
+		run_xsbck()
 		
-	except AbortException:
+	except AbortForHelpException:
 		pass
 	except Exception as e:
 		logger.error( f"Error: {e}" )
 	
 	## End
 	walltime1 = dt.datetime.utcnow()
+	logger.info(LINE)
 	logger.info( "End: {}".format(str(walltime1)[:19] + " (UTC)") )
 	logger.info( "Wall time: {}".format(walltime1 - walltime0) )
 	logger.info(LINE)
