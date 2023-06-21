bddbe3e60ab6d5e3537c091af3914bcc081dfb55
yrobin <yoann.robin.k@gmail.com>
2023-06-21 10:47:49 +0200

    version 1.1.0: change the way of XZarr to open data and minor modifications

--- XSBCK/__release.py
+++ XSBCK/__release.py
@@ -18,8 +18,8 @@
 
 
 version_major = "1"
-version_minor = "0"
-version_patch = "2"
+version_minor = "1"
+version_patch = "0"
 version_extra = ""
 version       = f"{version_major}.{version_minor}.{version_patch}{version_extra}"
 
